5def021ab5aee1e0b0f0d66b1e1705bd0f26affe
alschell <alexander-schell@live.de>
2025-07-11 13:00:41 +0800

    Create helpers.py

---
+++ utils/helpers.py
@@ -0,0 +1,10 @@
+import random
+import logging
+
+logger = logging.getLogger(__name__)
+
+def calculate_esg_score(portfolio):
+    return random.uniform(0.7, 0.95)
+
+def simulate_tax_optimization(proceeds):
+    return proceeds * random.uniform(0.8, 0.95)
